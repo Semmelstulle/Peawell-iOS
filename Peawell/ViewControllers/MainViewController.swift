909de3f8008f97530dc6ff99c4959e7f7ad09664
SemmelStulle <74740271+SemmelStulle@users.noreply.github.com>
2022-11-16 10:51:11 +0100

    bla

--- Peawell/ViewControllers/MainViewController.swift
+++ Peawell/ViewControllers/MainViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let navBar = UINavigationController()
+        navBar.title = "Peawell 2"
+        navBar.navigationBar.prefersLargeTitles = true
+
         view.backgroundColor = UIColor.systemBackground
+        label = UILabel(frame: CGRect(x: 10, y: 150, width: 300, height: 50))
+        label.text = "This is Peawell. Add your medication below.";
+        view.addSubview(label)
         
+        label = UILabel(frame: CGRect(x: 10, y: 500, width: 300, height: 50))
+        label.text = "Hola amigo";
+        view.addSubview(label)
         
     }
 
--- Peawell/ViewControllers/TabBarViewController.swift
+++ Peawell/ViewControllers/TabBarViewController.swift
@@ -20,10 +20,6 @@ class TabBarViewController: UITabBarController {
         let vc1 = ActionsViewController()
         let vc2 = ReminderViewController()
         let vc3 = SettingsViewController()
-        //  force large title
-//        vc1.navigationItem.largeTitleDisplayMode = .automatic
-//        vc2.navigationItem.largeTitleDisplayMode = .always
-//        vc3.navigationItem.largeTitleDisplayMode = .always
         //  set titles for the bar
         vc0.title = "Peawell"
         vc1.title = "Actions"
@@ -39,7 +35,6 @@ class TabBarViewController: UITabBarController {
         nav1.tabBarItem = UITabBarItem(title: "Actions", image: UIImage(systemName: "list.bullet"), tag: 1)
         nav2.tabBarItem = UITabBarItem(title: "Reminders", image: UIImage(systemName: "bell"), tag: 2)
         nav3.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gear"), tag: 3)
-
         //  force large title
         nav0.navigationBar.prefersLargeTitles = true
         nav1.navigationBar.prefersLargeTitles = true
