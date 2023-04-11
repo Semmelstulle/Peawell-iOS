ed27e8e77cc5d923544afc6114e472943b7d791b
SemmelStulle <74740271+SemmelStulle@users.noreply.github.com>
2023-04-11 19:05:46 +0200

    restart as SwifttUI

---
+++ Peawell/Peawell/PeawellApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeawellApp.swift
+//  Peawell
+//
+//  Created by Dennis on 11.04.23.
+//
+
+import SwiftUI
+
+@main
+struct PeawellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
