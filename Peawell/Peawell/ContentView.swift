96537724012495f5cd8aec22fc5d4bc96f968083
SemmelStulle <74740271+SemmelStulle@users.noreply.github.com>
2023-04-11 20:26:48 +0200

    basic screens

--- Peawell/Peawell/ContentView.swift
+++ Peawell/Peawell/ContentView.swift
@@ -7,16 +7,76 @@
 
 import SwiftUI
 
+//  Constants stored on the top
+let mainTitle: String = "Peawell"
+let histTitle: String = "History"
+let settingsTitle: String = "Settings"
+
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            MainView()
+                .navigationBarTitle("Test", displayMode: .inline)
+                .tabItem {
+                    Image(systemName: "iphone.homebutton")
+                    Text(mainTitle)
+                }
+            HistoryView()
+                .tabItem {
+                    Image(systemName: "calendar")
+                    Text(histTitle)
+                }
+            SettingsView()
+                .tabItem {
+                    Image(systemName: "gearshape.fill")
+                    Text(settingsTitle)
+                }
         }
         .padding()
+        
     }
+
+    struct MainView: View {
+        var body: some View {
+            VStack(
+                spacing: 20
+            ) {
+                Text("henlo");
+                HStack(
+                    spacing: 30
+                ) {
+                    RoundedRectangle(
+                        cornerRadius: 30,
+                        style: .continuous
+                    )
+                        .foregroundColor(.accentColor)
+                        .frame(width: 200, height: 200)
+                    RoundedRectangle(
+                        cornerRadius: 30,
+                        style: .continuous
+                    )
+                        .foregroundColor(.accentColor)
+                        .frame(width: 200, height: 200)
+                }
+                
+                
+                    
+            }
+            
+        }
+    }
+    struct HistoryView: View {
+        var body: some View {
+            Color.gray
+        }
+    }
+    struct SettingsView: View {
+        var body: some View {
+            Color.blue
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
