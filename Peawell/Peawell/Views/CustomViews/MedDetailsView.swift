8f539197b5fb6bf064d5b7282d58ac45e6939a47
SemmelStulle <74740271+SemmelStulle@users.noreply.github.com>
2023-04-17 18:55:39 +0200

    new view

---
+++ Peawell/Peawell/Views/CustomViews/MedDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  MedDetailsView.swift
+//  Peawell
+//
+//  Created by Dennis on 17.04.23.
+//
+
+import SwiftUI
+
+struct MedDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MedDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MedDetailsView()
+    }
+}
