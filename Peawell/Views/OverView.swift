46a42610ce0567c6addb866e24f392963214d41f
Semmelstulle <74740271+Semmelstulle@users.noreply.github.com>
2024-06-13 15:56:52 +0200

    fancier overview

--- Peawell/Views/OverView.swift
+++ Peawell/Views/OverView.swift
@@ -17,10 +17,10 @@ var bgColorAwesome: Color = Color.mint
 struct OverView: View {
     
     //  adds fetched data to scope
-    @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Mood.logDate, ascending: true)], animation: .default)
-    var moodItems: FetchedResults<Mood>
     @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Meds.medType, ascending: true)], animation: .default)
     var medsItems: FetchedResults<Meds>
+    @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Mood.logDate, ascending: false)], animation: .default)
+    var moodItems: FetchedResults<Mood>
     
     var body: some View {
         NavigationView {
@@ -38,23 +38,70 @@ struct OverView: View {
                 }
                 Section(header: Text(NSLocalizedString("mood section", comment: "tell the person this is the section containing their logged moods"))) {
                     ForEach(moodItems) { item in
-						NavigationLink {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .foregroundColor(Color.accentColor)
-                                    .frame(maxWidth: .infinity, maxHeight: 50)
+                        NavigationLink {
+                            ScrollView () {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .foregroundColor(Color.accentColor)
+                                        .frame(maxWidth: .infinity, idealHeight: 50)
+                                        .padding()
+                                    Image("mood\(item.moodName ?? "Neutral")")
+                                }
+                                Text(item.activityName ?? "Text missing")
+                                    .navigationTitle(Text(item.logDate ?? Date.now, style: .date))
+                                    .frame(maxWidth: .infinity, alignment: .topLeading)
+                                /*.background(Color.secondarySystemBackground)
+                                 .clipShape(RoundedRectangle(cornerRadius: 10))*/
                                     .padding()
-                                Image("mood\(item.moodName ?? "Neutral")")
                             }
-                            //Text(item.moodName ?? "Mood missing")
-                            Text(item.activityName ?? "Text missing")
-                                .navigationTitle(Text(item.logDate ?? Date.now, style: .date))
-                                .frame(maxWidth: .infinity, maxHeight: .infinity)
                         } label: {
-                            HStack() {
+                            /*HStack() {
                                 Text(item.logDate ?? Date.now, style: .date)
                                 Text(" - ")
                                 Text(item.moodName ?? "Mood missing")
+                            }*/
+                            HStack() {
+
+                                Image("mood\(item.moodName ?? "Neutral")")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                    .padding(6)
+                                    .background(Color.accentColor)
+                                /*
+                                if item.moodName == "Horrible" {
+                                    .background(Color.bgColorHorrible)
+                                }
+                                if item.moodName == "Bad" {
+                                    .background(Color.bgColorBad)
+                                }
+                                if item.moodName == "Neutral" {
+                                    .background(Color.bgColorNeutral)
+                                }
+                                if item.moodName == "Good" {
+                                    .background(Color.bgColorGood)
+                                }
+                                if item.moodName == "Awesome" {
+                                    .background(Color.bgColorAwesome)
+                                }
+                                */
+                                /*
+                                switch item.moodName ?? "Neutral" {
+                                case "Horrible":
+                                        .background(Color.bgColorHorrible)
+                                case "Bad":
+                                        .background(Color.bgColorBad)
+                                case "Neutral":
+                                        .background(Color.bgColorNeutral)
+                                case "Good":
+                                        .background(Color.bgColorGood)
+                                case "Awesome":
+                                        .background(Color.bgColorAwesome)
+                                default:
+                                        .background(Color.bgColorNeutral)
+                                }
+                                */
+                                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                                Text(item.logDate ?? Date.now, style: .date)
                             }
 						}
                     }
